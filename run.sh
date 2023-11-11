f483b11590278902a21973fe0fb65d422a621611
Fabio1988 <fabio.schett@gmail.com>
2023-11-12 00:34:21 +0100

    adapt run.sh

--- run.sh
+++ run.sh
@@ -36,41 +36,56 @@ fi
 GITHUB_OWNER="UnownHash"
 GITHUB_REPO="Dragonite-Public"
 
-# Get the selected file name from the argument
-FILE_NAME="dragonite-$FILE_PREFIX"
-
 # Fetch the latest tags from the local Git repository
 git fetch --tags
 
-# Get the latest Git tag for the GitHub repository
-latest_tag=$(git tag --list | sort -V | tail -n 1)
+# Get the latest Git tag for the "dragonite-" prefix
+latest_dragonite_tag=$(git tag --list 'dragonite-v*' | sort -V | tail -n 1)
+# Get the latest Git tag for the "admin-" prefix
+latest_admin_tag=$(git tag --list 'admin-v*' | sort -V | tail -n 1)
 
-# Define the latest Git tag
-RELEASE_TAG="$latest_tag"
+download_latest_release() {
+  local application="$1"
+  local FILE_NAME="$application-$FILE_PREFIX"
 
-# Get the list of releases using the GitHub API
-api_url="https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases/tags/$latest_tag"
-releases_info=$(curl -sf "$api_url")
+  # Set the correct variable based on the prefix
+  if [ "$application" == "dragonite" ]; then
+    local latest_tag_var="$latest_dragonite_tag"
+  elif [ "$application" == "admin" ]; then
+    local latest_tag_var="$latest_admin_tag"
+  else
+    echo "Invalid prefix: $prefix"
+    exit 1
+  fi
 
-if [ -z $releases_info ]; then
-  echo "Failed to download release information"
-  exit 1
-fi
+  # Get the list of releases using the GitHub API
+  local api_url="https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases/tags/$latest_tag_var"
+  local releases_info=$(curl -sf "$api_url")
 
-echo $releases_info
-# Extract the download URL for the specific file in the latest release
-download_url=$(echo "$releases_info" | jq -r ".assets[] | select(.name == \"$FILE_NAME\").url")
+  if [ -z $releases_info ]; then
+    echo "Failed to download $application release information"
+    exit 1
+  fi
 
-# Define the download file name based on the selected file
-download_filename="$FILE_NAME"
+  # Extract the download URL for the specific file in the latest release
+  local download_url=$(echo "$releases_info" | jq -r ".assets[] | select(.name == \"$FILE_NAME\").url")
 
-# Download the specific release file
-curl -L -H "Accept: application/octet-stream" -o "dragonite/$download_filename"  "$download_url"
+  # Define the download file name based on the selected file
+  local download_filename="$FILE_NAME"
 
-# Check if the download was successful
-if [ $? -ne 0 ]; then
+  # Download the specific release file
+  curl -L -H "Accept: application/octet-stream" -o "$application/$download_filename"  "$download_url"
+
+  # Check if the download was successful
+  if [ $? -ne 0 ]; then
     echo "Failed to download the release file"
     exit 1
-fi
+  fi
+  echo "Downloaded $download_filename"
+}
 
-echo "Downloaded $download_filename from the latest release with tag $RELEASE_TAG"
+download_latest_release "dragonite"
+download_latest_release "admin"
+
+echo "Latest release with tag $latest_dragonite_tag"
+echo "Latest release with tag $latest_admin_tag"
